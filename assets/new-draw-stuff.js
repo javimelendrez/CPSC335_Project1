609f7c646c9ca1dedfbfbdc1f367666a0757f921
Liam <liam.fitz85@yahoo.com>
2019-02-24 19:57:31 -0800

    basically finished.

---
+++ assets/new-draw-stuff.js
@@ -0,0 +1,106 @@
+var global_generation = 0;
+var columns = 40;
+
+function draw_rect(ctx, stroke, fill) {
+    stroke = stroke || 'lightgrey';
+    fill = fill || 'dimgrey';
+    ctx.save( );
+    ctx.strokeStyle = stroke;
+    ctx.fillStyle = fill;
+    ctx.lineWidth = 5;
+    ctx.rect(75, 50, canvas.width - 150, canvas.height - 100);
+    ctx.stroke();
+    ctx.fill();
+    ctx.restore( );
+}
+
+function draw_grid(rctx, rminor, rmajor, rstroke, rfill) {
+    rctx.save( );
+    rctx.strokeStyle = rstroke;
+    rctx.fillStyle = rfill;
+    let width = rctx.canvas.width;
+    let height = rctx.canvas.height;
+    for ( var ix = 0; ix < width; ix += rminor )
+    {
+        rctx.beginPath( );
+        rctx.moveTo( ix, 0 );
+        rctx.lineTo( ix, height );
+        rctx.lineWidth = ( ix % rmajor == 0 ) ? 0.5 : 0.25;
+        rctx.stroke( );
+        if ( ix % rmajor == 0 ) { rctx.fillText( ix, ix, 10 ); }
+    }
+    for ( var iy = 0; iy < height; iy += rminor )
+    {
+        rctx.beginPath( );
+        rctx.moveTo( 0, iy );
+        rctx.lineTo( width, iy );
+        rctx.lineWidth = ( iy % rmajor == 0 ) ? 0.5 : 0.25;
+        rctx.stroke( );
+        if ( iy % rmajor == 0 ) {rctx.fillText( iy, 0, iy + 10 );}
+    }
+    rctx.restore( );
+}
+
+function next_bitstring(bitstr) {
+
+    var new_bitstr = [];
+    for(var col = 0; col < columns; col++)
+    {
+        // lines 23 -> 34 determine the rules pattern
+        if(col-1 <= 0)
+            left = 0;
+        else
+            left = bitstr[col-1];
+
+        center = bitstr[col];
+
+        if(col+1 >= columns)
+            right = 0;
+        else
+            right = bitstr[col+1];
+
+        // this block deteremines the output
+        if(left == 1 && center == 1 && right == 1)
+            new_bitstr[col] = 0;
+        else if(left == 1 && center == 1 && right == 0)
+            new_bitstr[col] = 1;
+        else if(left == 1 && center == 0 && right == 1)
+            new_bitstr[col] = 0;
+        else if(left == 1 && center == 0 && right == 0)
+            new_bitstr[col] = 1;
+        else if(left == 0 && center == 1 && right == 1)
+            new_bitstr[col] = 1;
+        else if(left == 0 && center == 1 && right == 0)
+            new_bitstr[col] = 0;
+        else if(left == 0 && center == 0 && right == 1)
+            new_bitstr[col] = 1;
+        else
+            new_bitstr[col] = 0;
+    }       
+
+    return new_bitstr;
+}
+
+function draw_cell(ctx, x, y) {
+    var stroke = "transparent";
+    var fill = "red";
+    ctx.save();
+    ctx.strokeStyle = stroke;
+    ctx.fillStyle = fill;
+    ctx.lineWidth = 0;
+    width = canvas.width - 210;
+    ctx.rect(x,y,8.5,8.5);
+    ctx.fill();
+    ctx.restore();
+}
+
+function draw_rule(ctx, arr) {
+    for(var i = 0; i < arr.length; i++)
+    {
+        if(arr[i] == 1)
+        {
+            draw_cell(ctx, x, global_generation);
+        }
+    }
+    global_generation += 10
+}
