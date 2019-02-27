d6a5211e8d18064416240bafb98fb71908b5243f
liamfitz85 <liam.fitz85@yahoo.com>
2019-02-27 13:35:27 -0800

    most recent commit. after meeting with the professor.

--- assets/new-draw-stuff.js
+++ assets/new-draw-stuff.js
@@ -1,19 +1,6 @@
 var global_generation = 0;
 var columns = 40;
 
-function draw_rect(ctx, stroke, fill) {
-    stroke = stroke || 'lightgrey';
-    fill = fill || 'dimgrey';
-    ctx.save( );
-    ctx.strokeStyle = stroke;
-    ctx.fillStyle = fill;
-    ctx.lineWidth = 5;
-    ctx.rect(75, 50, canvas.width - 150, canvas.height - 100);
-    ctx.stroke();
-    ctx.fill();
-    ctx.restore( );
-}
-
 function draw_grid(rctx, rminor, rmajor, rstroke, rfill) {
     rctx.save( );
     rctx.strokeStyle = rstroke;
@@ -27,7 +14,7 @@ function draw_grid(rctx, rminor, rmajor, rstroke, rfill) {
         rctx.lineTo( ix, height );
         rctx.lineWidth = ( ix % rmajor == 0 ) ? 0.5 : 0.25;
         rctx.stroke( );
-        if ( ix % rmajor == 0 ) { rctx.fillText( ix, ix, 10 ); }
+        //if ( ix % rmajor == 0 ) { rctx.fillText( ix, ix, 10 ); }
     }
     for ( var iy = 0; iy < height; iy += rminor )
     {
@@ -36,7 +23,7 @@ function draw_grid(rctx, rminor, rmajor, rstroke, rfill) {
         rctx.lineTo( width, iy );
         rctx.lineWidth = ( iy % rmajor == 0 ) ? 0.5 : 0.25;
         rctx.stroke( );
-        if ( iy % rmajor == 0 ) {rctx.fillText( iy, 0, iy + 10 );}
+        //if ( iy % rmajor == 0 ) {rctx.fillText( iy, 0, iy + 10 );}
     }
     rctx.restore( );
 }
@@ -44,20 +31,27 @@ function draw_grid(rctx, rminor, rmajor, rstroke, rfill) {
 function next_bitstring(bitstr) {
 
     var new_bitstr = [];
-    for(var col = 0; col < columns; col++)
+    for(var col = 0; col < bitstr.length; col++)
     {
         // lines 23 -> 34 determine the rules pattern
-        if(col-1 <= 0)
-            left = 0;
-        else
-            left = bitstr[col-1];
-
-        center = bitstr[col];
-
-        if(col+1 >= columns)
-            right = 0;
-        else
-            right = bitstr[col+1];
+		if(col == 0) {
+			left = 0;
+			center = bitstr[col];
+			right = bitstr[col+1];
+			//console.log("col: ", col);
+		}
+		else if(col == columns - 1)
+		{
+			left = bitstr[col-1];
+			center = bitstr[col];
+			right = 0;
+			//console.log("col: ",col);
+		} else {
+			left = bitstr[col-1];
+			center = bitstr[col];
+			right = bitstr[col+1];
+			//console.log	("col: ",)
+		}
 
         // this block deteremines the output
         if(left == 1 && center == 1 && right == 1)
@@ -74,7 +68,7 @@ function next_bitstring(bitstr) {
             new_bitstr[col] = 0;
         else if(left == 0 && center == 0 && right == 1)
             new_bitstr[col] = 1;
-        else
+        else if(left == 0 && center == 0 && right == 0)
             new_bitstr[col] = 0;
     }       
 
@@ -92,15 +86,4 @@ function draw_cell(ctx, x, y) {
     ctx.rect(x,y,8.5,8.5);
     ctx.fill();
     ctx.restore();
-}
-
-function draw_rule(ctx, arr) {
-    for(var i = 0; i < arr.length; i++)
-    {
-        if(arr[i] == 1)
-        {
-            draw_cell(ctx, x, global_generation);
-        }
-    }
-    global_generation += 10
 }
